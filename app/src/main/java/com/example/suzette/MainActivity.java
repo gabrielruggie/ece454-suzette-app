6b123b8e25f8edfd1c635bd005afdc307c3f8262
dooj327 <djevtic@wisc.edu>
2024-04-17 16:24:40 -0500

    Added recipeActivity Functionality into MainActivity

--- app/src/main/java/com/example/suzette/MainActivity.java
+++ app/src/main/java/com/example/suzette/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.suzette;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+// RecipeActivity.java
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Array of sample texts
+    private String[] sampleTexts = {
+            "Sample Text 1",
+            "Sample Text 2",
+            "Sample Text 3",
+            "Sample Text 4",
+            "Sample Text 5",
+            "Sample Text 6",
+            "Sample Text 7",
+            "Sample Text 8",
+            "Sample Text 9",
+            "Sample Text 10"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_activity);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        for (String text : sampleTexts) {
+            ItemFragment fragment = ItemFragment.newInstance(text);
+            fragmentTransaction.add(R.id.container, fragment);
+        }
+
+        fragmentTransaction.commit();
     }
 }
